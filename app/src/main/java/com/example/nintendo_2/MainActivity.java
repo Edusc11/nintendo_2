e77b18ff61172932e10bfc615f8159cd10068cd8
senai <evelyn.quispe@aluno.senai.br>
2026-08-20 17:08:26 -0300

    testando

---
+++ app/src/main/java/com/example/nintendo_2/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.nintendo_2;
+
+import android.content.Intent; // Importação necessária para mudar de página
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button; // Importação necessária para usar o Botão
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button stfox = findViewById(R.id.starfox);
+
+        stfox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, starfox.class);
+                startActivity(intent);
+            }
+        });
+
+        Button conke = findViewById(R.id.conkers);
+
+        conke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, conkers.class);
+                startActivity(intent);
+            }
+        });
+
+        Button dk = findViewById(R.id.kong);
+
+        dk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, personagensDK.class);
+                startActivity(intent);
+            }
+        });
+
+        Button lk = findViewById(R.id.lonk);
+
+        lk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, personagensLK.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
